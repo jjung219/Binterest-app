be92d03a6aac82ce64daf5ac43e59963a35131f8
jjung219 <62397536+jjung219@users.noreply.github.com>
2020-07-27 16:50:06 -0400

    used unsplash API to get images and added ImageList file to show images below the search box

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
+import ImageList from './ImageList'
 
-const App = () => {
+class App extends React.Component {
+    state = { images: [] }
 
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID HogGshsiM9hbZC7IBAJ1opNRJb9aWwQEoEVrCsxxYq8'
+            }
+        })
+        this.setState({ images: response.data.results })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList  images={this.state.images} />
+            </div>
+        )
+    }
 
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term)//submit 
+    }
+
     render() {
         return (
             <div>
                 <div className="ui placeholder segment">
-                    <form className="ui form">
+                    <form onSubmit={this.onFormSubmit} className="ui form">
                         <div className="field">
                             <label>
                                 Image Search
                                 <div className="ui icon input">
-                                    <input type="prompt" placeholder="Search images..."/>
-                                    <i class="search icon"></i>
+                                    <input type="prompt" placeholder="Search images..."
+                                    value={this.state.term}
+                                    onChange={(e) => this.setState({ term: e.target.value })}
+                                    />
+                                    <i className="search icon"></i>
                                 </div>
                             </label>
                         </div>
